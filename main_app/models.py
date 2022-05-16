50ec26cda552391340a1d23d3d2656eb5bcb5c1a
jhuang15 <jchuang1020@gmail.com>
2022-05-16 14:46:02 -0400

    Reverting back to previous commit

--- main_app/models.py
+++ main_app/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 from django.urls import reverse
-
+from django.contrib.auth.models import User
 
 
 
 class Cake(models.Model):
     name = models.CharField(max_length=100)
-    type = models.CharField(max_length=100)
-    description = models.TextField(max_length=300)
+    creation_date = models.DateField()
+    description = models.TextField(max_length=600)
+    recipe = models.TextField(max_length=600)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
