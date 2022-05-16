ee981ee9acd2887ed9854729e9fdcee070110323
jhuang15 <jchuang1020@gmail.com>
2022-05-16 16:51:17 -0400

    Minor field changes, removed 'types'

--- main_app/views.py
+++ main_app/views.py
@@ -11,7 +11,7 @@ class CakeCreate(CreateView):
 
 class CakeUpdate(UpdateView):
   model = Cake
-  fields = ['type', 'description']
+  fields = ['description', 'recipe']
 
 class CakeDelete(DeleteView):
   model = Cake
